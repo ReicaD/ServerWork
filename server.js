c6469dbaa084729d25e832aa1725419e334208d7
ReicaD <dbamanya447@gmail.com>
2022-10-03 08:31:42 -0400

    Adding server for IP adress and portnumbers WIP

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 //this is used to create a server
-const http = require('http');
+const http = require("http");
 
 //by adding a constant its going to be installed request is used to reque
- const server = http.createServer((req , res) => {
-     //this method will return get
-console.log(req.url, req.method);
-//setting header content type
-req.setHeader('Content-Type', 'text/plain');
+const server = http.createServer((req, res) => {
+  //this method will return get
+  console.log(req.url, req.method);
+  //setting header content type
+  res.setHeader("Content-Type", "text/plain");
+ 
+  res.write("We are all one, human");
+  res.end();
+});
 
-
-res.write('We are all one ', 'human');
-res.end();
- });
-
-
-server.listen(3000,'localhost',() => {
- console.log('listening for requests on port 3000');
+const PORT = 3000;
+server.listen(PORT, "localhost", () => {
+  console.log(`listening for requests on port ${PORT}`);
 });

dc9b6c826eb228a7e0513f443247c57a43aa4d63
ReicaD <dbamanya447@gmail.com>
2022-10-05 22:02:37 -0400

    added package loc json WIP

--- Views/server.js
+++ Views/server.js
@@ -13,12 +13,24 @@ const server = http.createServer((req, res) => {
   switch(req.url){
     case '/': 
     path += 'index.html';
+    res.statusCode = 200;
     break;
     case '/about':
       path += 'about.html';
+      res.statusCode = 200;
       break;
+
+      case '/about-me':
+      path += 'about.html';
+      res.statusCode = 301;
+      res.setHeader('Location', '/about')
+      res.end()
+      break;
+
+
       default:
         path += '404.html';
+        res.statusCode = 404;
         break;
   }
 
@@ -35,7 +47,9 @@ const server = http.createServer((req, res) => {
     } else {
       res.write(data);
       //res.write("<p>The world is, us</p>");
-      res.end();
+      //writing status codes 
+       
+      res.end(data);
     }
   });
 });

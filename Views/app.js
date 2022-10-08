bf0938e32c582fcb329ef29509e40fe5c3b1ee80
ReicaD <dbamanya447@gmail.com>
2022-10-08 11:06:53 -0400

    added express work  WIP

--- Views/app.js
+++ Views/app.js
@@ -1,19 +1,20 @@
-const express = require('express')
-
+const express = require("express");
 
 //this helps to read the express files
 const app = express();
 
-
 //listening to requests
 app.listen(3000);
 
-app.get('/',(req,res)=> {
-//res .sendFile('./Views/404.html')
+app.get("/", (req, res) => {
+  //res .sendFile('./Views/404.html')
+  res.send("<p>Home page</p>");
+
+  //   app.get("/about", (req, res) => {
+  //     //res.send('<p>Home page</p>');
+  //   });
+});
+
+app.get("/about", (req, res) => {
   //res.send('<p>Home page</p>');
-
-app.get('/about',(req,res)=> {
-
-  //res.send('<p>Home page</p>');
-
-}
+});

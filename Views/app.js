e6ab33f5caa58133cae3a5cca8353102dfcdfbb1
ReicaD <dbamanya447@gmail.com>
2022-10-09 02:10:10 -0400

    added ejs and files.ejs  WIP

--- Views/app.js
+++ Views/app.js
@@ -3,18 +3,33 @@ const express = require("express");
 //this helps to read the express files
 const app = express();
 
+//register view engine
+app.set('view engine','ejs');
+
 //listening to requests
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  //res .sendFile('./Views/404.html')
-  res.send("<p>Home page</p>");
+    //here __dirname gets the directory that we are in the index.html file
+  res.sendFile("../Views/index.html",{root: __dirname});
+  //res.send("<p>Home page</p>");
 
   //   app.get("/about", (req, res) => {
   //     //res.send('<p>Home page</p>');
   //   });
+
+  app.get("/about-us",(req,res)=>{
+      res.redirect("/about")
+  })
 });
 
 app.get("/about", (req, res) => {
+    res.sendFile("../Views/about.html",{root: __dirname});
   //res.send('<p>Home page</p>');
+   
+});
+
+//use is used for middleware functions in express 404 page
+app.use((req,res)=>{
+    res.status(404).sendFile("../Views/404.html",{root: __dirname})
 });

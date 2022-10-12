d394d9bd7d1c67cd484c34fce1738071f4a0e0f8
ReicaD <dbamanya447@gmail.com>
2022-10-12 08:10:53 -0400

    added ejs HTML files  WIP

--- Views/app.js
+++ Views/app.js
@@ -1,35 +1,42 @@
 const express = require("express");
 
+const PORT = 3000;
+
 //this helps to read the express files
 const app = express();
 
 //register view engine
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 //listening to requests
-app.listen(3000);
+app.listen(PORT, "localhost", () => {
+  console.log(`listening for requests on port ${PORT}`);
+});
+
+   
 
 app.get("/", (req, res) => {
     //here __dirname gets the directory that we are in the index.html file
-  res.sendFile("../Views/index.html",{root: __dirname});
+  res.render('index');
   //res.send("<p>Home page</p>");
 
   //   app.get("/about", (req, res) => {
   //     //res.send('<p>Home page</p>');
   //   });
+  });
 
-  app.get("/about-us",(req,res)=>{
-      res.redirect("/about")
-  })
-});
+  app.get("/about", (req,res)=>{
+      res.render("/about");
+  });
 
-app.get("/about", (req, res) => {
-    res.sendFile("../Views/about.html",{root: __dirname});
+
+app.get("/about-us", (req, res) => {
+    res.redirect("about");
   //res.send('<p>Home page</p>');
    
 });
 
 //use is used for middleware functions in express 404 page
-app.use((req,res)=>{
-    res.status(404).sendFile("../Views/404.html",{root: __dirname})
+app.use((req,res) => {
+    res.status(404).render('404');  
 });

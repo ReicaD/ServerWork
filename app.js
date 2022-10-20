b9da28f932f2de305f88e5d3fc0cffd24577ce23
ReicaD <dbamanya447@gmail.com>
2022-10-20 14:53:53 -0400

    Added functionality code tips

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 const express = require("express");
-
-const PORT = 3000;
-
-//this helps to read the express files
 const app = express();
-
+const morgan = require('morgan');
 //register view engine
 app.set("view engine", "ejs");
+
+const PORT = 3000;
+//we add next to make sure that our function above can be able to work/render on the bowser
+app.use((req, res, next) => {
+  console.log("new request made:");
+  console.log("host:", req.hostname);
+  console.log("path:", req.path);
+  console.log("method:", req.method);
+  next();
+});
+app.use((req, res, next) => {
+  console.log("in the next middleware:");
+  next();
+});
+//this helps to read the express files
+// const app = express();
 
 app.get("/", (req, res) => {
   // here __dirname gets the directory that we are in the index.html file
   const blogs = [
-    {tittle: 'Money Rules', snippet:'All You Need to know'},
-    {tittle: 'All regulations', snippet:'See more'},
-    {tittle: 'See tricks', snippet:'About money life'},
-    
+    { tittle: "Money Rules", snippet: "All You Need to know" },
+    { tittle: "All regulations", snippet: "See more" },
+    { tittle: "See tricks", snippet: "About money life" },
   ];
   res.render("index", { tittle: "Home", blogs });
   //res.send("<p>Home page</p>");
@@ -23,12 +34,11 @@ app.get("/", (req, res) => {
   //     //res.send('<p>Home page</p>');
   //   });
 });
-
 app.get("/about", (req, res) => {
   res.render("about", { tittle: "about" });
 });
 
-// app.get("/about-us", (req, res) => {
+// app.get("/about-us", (r eq, res) => {
 //   res.redirect("/about");
 //   //res.send('<p>Home page</p>');
 // });

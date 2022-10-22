693ab5f4385043d6701f9875d81e068f4645c19d
ReicaD <dbamanya447@gmail.com>
2022-10-22 03:20:16 -0400

    Installed Mongoose and added model file

--- app.js
+++ app.js
@@ -1,16 +1,44 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require('./models/blog')
+
 //this helps to read the express files
 const app = express();
-//register view engine
-app.set("view engine", "ejs");
 
-const PORT = 3000;
+//adding mongolAtlas File that connects to mongoldb
+const dbURI =
+  "mongodb+srv://Node-Tuts1:NodeToGo225@serverworksss.m7ixa5s.mongodb.net/Node-Tuts1?retryWrites=true&w=majority";
+
+//this function below helps to connect database created. the added argument is to stop deprication warnings
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+//register view engine
+app.set("view engine", "ejs")
 
 //middle ware and static files
-app.use(express.static('public'));
-app.use(morgan('dev'));
+app.use(express.static("public"));
+app.use(morgan("dev"));
 
+//mongoose and mongo sandbox routes
+
+app.get('./add-blog',(req, res) =>{
+const blog = new Blog ({
+  tittle:'new blog',
+  snippet:'about money blog',
+ body:'How to Save Money'
+});
+
+blog.save()
+.then((result)=>{
+  res.send(result)
+}) 
+.catch(err)
+console.log(err)
+})
 
 app.get("/", (req, res) => {
   // here __dirname gets the directory that we are in the index.html file
@@ -45,6 +73,6 @@ app.use((req, res) => {
 });
 
 //register view engine
-app.listen(PORT, "localhost", () => {
-  console.log(`listening for requests on port ${PORT}`);
-});
+// app.listen(PORT, "localhost", () => {
+//   console.log(`listening for requests on port ${PORT}`);
+// });
---
+++ models/blog.js
@@ -0,0 +1,28 @@
+//mongoose has to be required first
+const mongoose = require('mongoose')
+
+//require the schema too of where the data will be written and passed
+const Schema = mongoose.Schema;
+
+//creating the Blog Schema using properties of tittle,snippet,body or etc
+
+const blogSchema = new Schema({
+    tittle:{
+        type:String,
+        required: true
+    },
+snippet:{
+    type:String,
+    required: true
+},
+body:{
+    type:String,
+    required:true
+}
+//this generates timestamps for blog document  any time the objesct is created its going to auto value those properties
+},{timestamps:true});
+
+
+const Blog = mongoose.model('Blog', blogSchema);
+module.exports = Blog;
+
